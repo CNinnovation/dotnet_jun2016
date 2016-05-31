ff7b7824581acb35dbae156a5806e0ff4ef49f03
Christian Nagel <christian@christiannagel.com>
2016-05-31 16:29:46 +0200

    configuration sample

---
+++ ConfigurationFilesSample/src/ConfigurationFilesSample/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationFilesSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Config = new ConfigurationBuilder()
+                .SetBasePath(@"C:\Source\dotnet_jun2016\ConfigurationFilesSample\src\ConfigurationFilesSample\")
+                 .AddJsonFile("mymandatoryconfig.json")
+                 .AddJsonFile("myoptionalconfig.json", optional: true)
+                 .AddEnvironmentVariables()
+                 .AddCommandLine(args)
+                 .AddUserSecrets()
+                 .Build();
+
+            Run();
+
+        }
+
+        private static void Run()
+        {
+            string val1 = Config["SomeKey"];
+            string val2 = Config["ConnectionStrings:DefaultConnection"];
+
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+
+            string val3 = Config["a"];
+            Console.WriteLine(val3);
+
+            string val4 = Config["mysecret"];
+            Console.WriteLine(val4);
+
+        }
+
+        public static IConfigurationRoot Config { get; private set; }
+    }
+}
