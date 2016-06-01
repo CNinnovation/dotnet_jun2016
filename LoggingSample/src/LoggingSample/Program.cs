7bb2cc7867fa6b93c80b9ab3c7a48623f2f073c5
Christian Nagel <christian@christiannagel.com>
2016-06-01 20:59:03 +0200

    logging sample

---
+++ LoggingSample/src/LoggingSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggingSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ILoggerFactory logger = new LoggerFactory()
+                .AddConsole()
+                .AddDebug();
+
+            Logger = logger.CreateLogger<Program>();
+
+            Foo();
+
+        }
+
+        private static void Foo()
+        {
+            Logger.LogInformation($"{nameof(Foo)}");
+            Bar();
+        }
+
+        private static void Bar()
+        {
+            Logger.LogError($"Error in {nameof(Bar)}");
+        }
+
+        public static ILogger Logger { get; private set; }
+    }
+}
