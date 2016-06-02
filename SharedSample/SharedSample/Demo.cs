0ed753fc8b285890143b0eb5f9b1a57ddaced421
Christian Nagel <christian@christiannagel.com>
2016-06-02 07:21:31 +0200

    add code sharing sample

---
+++ SharedSample/SharedSample/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace SharedSample
+{
+    public class Demo
+    {
+        public void ShoWMessage(string message)
+        {
+#if WPF
+            MessageBox.Show(message);
+#else
+            Console.WriteLine(message);
+#endif
+        }
+
+    }
+}
